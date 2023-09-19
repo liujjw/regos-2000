45d7e375bb3ff4206a926e9bb5518c3546b70911
Jackie <jackyli742@gmail.com>
2023-09-19 00:58:59 -0400

    save

--- rusty_c/mydisk/src/TreediskFS_RS/DictFS.py
+++ rusty_c/mydisk/src/TreediskFS_RS/DictFS.py
@@ -2,9 +2,9 @@ import random
 import string
 
 num_inodes = 100
-max_offset = 100
+max_offset = 10
 num_operations = 100
-num_bytes = 32
+num_bytes = 16
 
 class DictFS:
   def __init__(self, num_inodes):
@@ -19,15 +19,20 @@ class DictFS:
       return None
     return self.mp[ino][offset]
   
+  def read_all(self, ino):
+    if ino not in self.mp:
+      return None
+    return self.mp[ino]
+  
   def write(self, ino, offset, data):
     if ino not in self.mp:
-      arr = [None] * offset
+      arr = [b'\x00'] * offset
       arr.append(data)
       self.mp[ino] = arr
     else:
       arr = self.mp[ino]
       if offset >= len(arr):  
-        arr.extend([None] * (offset - len(arr)))
+        arr.extend([b'\x00'] * (offset - len(arr)))
         arr.append(data)
       else:
         arr[offset] = data
@@ -65,9 +70,9 @@ def driver():
   for op in ops:
     if op[0] == "write":
       inode, offset = op[1], op[2]
-      data = encode(generate_random_string(random.randint(10,32)), num_bytes)
+      data = encode(generate_random_string(random.randint(num_bytes // 2, num_bytes)), num_bytes)
       fs.write(inode, offset, data)
-      res.append(fs.read(inode, offset))      
+      res.append(fs.read_all(inode))      
   print(res)
 
 driver()
